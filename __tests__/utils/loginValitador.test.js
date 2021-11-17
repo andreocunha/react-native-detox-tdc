0448a6ea37a5b9372f9aa309904cc71384340d07
Natalia Kelim Thiel <natalia.kthiel@gmail.com>
2021-11-17 15:40:22 -0300

    Add valid and invalid passwords

--- __tests__/utils/loginValitador.test.js
+++ __tests__/utils/loginValitador.test.js
@@ -1,7 +1,6 @@
 import { isValidEmail, isValidPassword } from '../../src/utils/loginValidator';
 
-describe('loginValidator.js', () => {
-  const VALID_EMAIL = 'valid@email.com';
+describe('loginValidator.js > isValidEmail()', () => {
   const VALID_EMAILS = [
     'email@example.com',
     'firstname.lastname@example.com',
@@ -42,6 +41,45 @@ describe('loginValidator.js', () => {
   it('given invalid emails, isValidEmail should return false', () => {
     INVALID_EMAILS.forEach((email) => {
       const validation = isValidEmail(email);
+      expect(validation).toBe(false);
+    });
+  });
+});
+
+describe('loginValidator.js > isValidPassword()', () => {
+  const VALID_PASSWORDS = [
+    'Mn1#aa',
+    '8Fp$oo',
+    ']sTRPLv6',
+    'B6,xoo^S',
+    '$4BfAm&i',
+    'BWxd,9\\4',
+    'Jhr4AiJ{',
+    '3PV/X(cx',
+  ];
+
+  const INVALID_PASSWORDS = [
+    'ZGimMtJ7',
+    'gqHTmDYj',
+    'GVYZSCWD',
+    'vgzjizxt',
+    '3mk2yhxd',
+    '<7v\')e"7',
+    '8}6_.&"-',
+    '_~^\'/(?#',
+    '*~>irs~$',
+  ];
+
+  it('given an valid password, isValidPassword should return true', () => {
+    VALID_PASSWORDS.forEach((password) => {
+      const validation = isValidPassword(password);
+      expect(validation).toBe(true);
+    });
+  });
+
+  it('given invalid passwords, isValidPassword should return false', () => {
+    INVALID_PASSWORDS.forEach((password) => {
+      const validation = isValidPassword(password);
       expect(validation).toBe(false);
     });
   });
